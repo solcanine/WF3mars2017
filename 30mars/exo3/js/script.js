885a40b940005c50229bd9cb47258402db3f616f
solcanine <solcanine66@gmail.com>
2017-03-30 11:05:06 +0200

    30mars

---
+++ 30mars/exo3/js/script.js
@@ -0,0 +1,17 @@
+// Sélecteur de balise HTML (tag)
+var myParaTag=document.getElementsByTagName('p');
+console.log(myParaTag);
+
+// Sélecteur de class
+var myClass=document.getElementsByClassName('myClass');
+console.log(myClass);
+
+// Selecteur d'ID
+var myId=document.getElementById('myId');
+console.log(myId);
+
+// Selecteur querySelector
+console.log(document.querySelector('h1') );
+
+// Selecteur querySelectorAll
+console.log(document.querySelectorAll('.myClass'));
