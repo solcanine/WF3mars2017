49c2b2d466fbac5aa9fab9dabe030158b2213d37
solcanine <solcanine66@gmail.com>
2017-04-04 17:15:34 +0200

    04avril

---
+++ Avril/04avril/exo7/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    // Capter le clic sur le premier bouton
+    $('button:first').click(function(){
+
+        // Charger le fichier home.html dans le main
+        $('main').load('views/home.html')
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}) //Fin du chargement du DOM
