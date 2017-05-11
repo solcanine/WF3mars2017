c08e991833226b9145f52796ccb9b304d201cbb7
solcanine <solcanine66@gmail.com>
2017-05-11 17:20:38 +0200

    11 Mai

---
+++ Wordpress/wordpress/wp-content/themes/eprojet/HEADER.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head <?php language_attributes(); ?>>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title <?php bloginfo('cname'); wp_title('_', true, 'left'); ?>>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
